972426379627d80768fa1b29d39862f00be1af3d
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-15 22:45:09 -0300

    Add StringValidatableExtensions

---
+++ AngelStack.DomainDrivenDesign.Abstractions/Extensions/StringValidatableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using AngelStack.Common.Guards;
+using AngelStack.Common.Strings;
+
+namespace AngelStack.DomainDrivenDesign.Abstractions.Extensions;
+
+public static class StringValidatableExtensions
+{
+    public static bool IsRequired<T>() where T : StringValidatable
+    {
+        return typeof(T).Guard().GetCustomAttribute<Required>()?.Value ?? false;
+    }
+
+    public static int? GetMinLength<T>()
+    {
+        return typeof(T).Guard().GetCustomAttribute<MinLength>()?.Value;
+    }
+
+    public static int? GetMaxLength<T>()
+    {
+        return typeof(T).GetCustomAttribute<MaxLength>()?.Value;
+    }
+
+    public static string? GetRegularExpression<T>()
+    {
+        return typeof(T).Guard().GetCustomAttribute<RegularExpression>()?.Value;
+    }
+}
